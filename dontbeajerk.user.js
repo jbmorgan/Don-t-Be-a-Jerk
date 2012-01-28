7b1f11dae64b7dad232fa31d1c090ade67f2f48a
Jonathan Morgan <jbmorgan@ncsu.edu>
2012-01-28 09:51:46 -0500

    Added comments

--- dontbeajerk.user.js
+++ dontbeajerk.user.js
@@ -1,15 +1,16 @@
 // ==UserScript==
 // @name          Don't Be a Jerk.
-// @namespace     http://www.theinterwebs.kom
+// @namespace     http://www.theinterwebs.com
 // @description	  Encourages you not to be an jerk online.
 // @author        Jonathan Morgan
-// @homepage      http://www.theinterwebs.kom/
+// @homepage      http://www.theinterwebs.com
 
 // ==/UserScript==
 var alreadyWarned = false;
-getAllForms();
+addEventListeners();
 
-function getAllForms() {
+//adds an event listener to every textarea on the current page
+function addEventListeners() {
     for(i=0; i < document.forms.length; i++) {
         for(j=0; j < document.forms[i].elements.length; j++) {
             if(document.forms[i].elements[j].type == "textarea") {
@@ -19,6 +20,8 @@ function getAllForms() {
     }
 }
 
+//warns the user to not be a jerk
+//only runs once per page
 function warn() {
     if(!alreadyWarned) 
         alert("Don't be a jerk."); 
